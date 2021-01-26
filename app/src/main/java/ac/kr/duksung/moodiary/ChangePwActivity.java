fd8636a30656d68f6cf255759a783e2a382596de
seungyeoni <dnri33@naver.com>
2021-01-26 15:26:23 +0900

    Add: Notification/ChangePw액티비티 추가, MyPage Fragment 수정(1)

--- app/src/main/java/ac/kr/duksung/moodiary/ChangePwActivity.java
+++ app/src/main/java/ac/kr/duksung/moodiary/ChangePwActivity.java
@@ -1,4 +1,40 @@
 package ac.kr.duksung.moodiary;
 
-public class ChangePwActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+
+// 화면 설명 : 마이페이지 안에 비밀번호 변경 화면
+// Author : Seungyeon, Last Modified : 2021.01.25
+public class ChangePwActivity extends AppCompatActivity {
+    Toolbar toolbar; // 상단바
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_pw);
+
+        // 상단바 설정
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false); //기본 타이틀 삭제
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); //back버튼
+
+        Intent Change = getIntent();    //화면 전환
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:{ //상단바의 back키 눌렀을 때
+                finish();
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
