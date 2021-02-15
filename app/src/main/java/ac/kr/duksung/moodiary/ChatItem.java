a86d806248e36138eba18a2b70463e271d04b665
soohyun <soohyun6879@naver.com>
2021-02-15 18:01:47 +0900

    Add: 챗봇 관련 class

---
+++ app/src/main/java/ac/kr/duksung/moodiary/ChatItem.java
@@ -0,0 +1,19 @@
+package ac.kr.duksung.moodiary;
+
+public class ChatItem {
+    private int type; // 뷰 타입 (챗봇 - 0, 사용자 - 1)
+    private String content; // 내용
+
+    public ChatItem(int type, String content) {
+        this.type = type;
+        this.content = content;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
