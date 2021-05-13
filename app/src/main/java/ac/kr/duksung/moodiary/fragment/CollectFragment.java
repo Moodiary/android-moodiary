b3ab4689a4b58b6f85485ed83b77f459c2f7f30e
soohyun <soohyun6879@naver.com>
2021-05-13 16:22:09 +0900

    Fix: deleteButton

--- app/src/main/java/ac/kr/duksung/moodiary/fragment/CollectFragment.java
+++ app/src/main/java/ac/kr/duksung/moodiary/fragment/CollectFragment.java
@@ -64,6 +64,8 @@ public class CollectFragment extends Fragment {
         diary_content = view.findViewById(R.id.diary_content);
         deleteButton = view.findViewById(R.id.deleteButton);
 
+        deleteButton.setVisibility(View.GONE);
+
         requestCollect(); // 일기 데이터 메소드 실행
 
         diary_calendar.setOnDateChangedListener(new OnDateSelectedListener(){
@@ -103,6 +105,7 @@ public class CollectFragment extends Fragment {
                     @Override
                     public void onClick(View view) {
                         deletediary(created_at);
+                        deleteButton.setVisibility(View.GONE);
                     }
                 });
             }
@@ -116,6 +119,7 @@ public class CollectFragment extends Fragment {
                 String nowStr = format.format(now);
 
                 deletediary(nowStr);
+                deleteButton.setVisibility(View.GONE);
             }
         });
 
@@ -146,7 +150,7 @@ public class CollectFragment extends Fragment {
 
         // 서버에 데이터 전달
 
-        JsonObjectRequest jsonObject = new JsonObjectRequest(Request.Method.POST, "http://172.30.1.36:3000/diary/collect", requestJsonObject, new Response.Listener<JSONObject>() {
+        JsonObjectRequest jsonObject = new JsonObjectRequest(Request.Method.POST, "http://172.30.1.9:3000/diary/collect", requestJsonObject, new Response.Listener<JSONObject>() {
 
 
             @Override
@@ -213,7 +217,7 @@ public class CollectFragment extends Fragment {
 
         // 서버에 데이터 전달
 
-        JsonObjectRequest jsonObject = new JsonObjectRequest(Request.Method.POST, "http://172.30.1.36:3000/diary/deletediary", requestJsonObject, new Response.Listener<JSONObject>() {
+        JsonObjectRequest jsonObject = new JsonObjectRequest(Request.Method.POST, "http://172.30.1.9:3000/diary/deletediary", requestJsonObject, new Response.Listener<JSONObject>() {
 
 
             @Override
@@ -265,7 +269,6 @@ public class CollectFragment extends Fragment {
 
             for(int i=0; i<createdList.size(); i++) {
                 Date createdDate = format.parse(createdList.get(i));
-                deleteButton.setVisibility(View.GONE);
 
                 if(createdDate.equals(nowDate)) {
                     diary_emotion.setText(emotionList.get(i));
